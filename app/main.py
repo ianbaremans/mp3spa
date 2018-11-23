172d00e078a78b18e1847416a3a5b541ffc4f3d7
ianbaremans <ianbaremans@gmail.com>
2018-11-23 22:02:16 +0100

    Add return file function

--- app/main.py
+++ app/main.py
@@ -1,25 +1,26 @@
 import os
 import flask 
 import stagger
+import json
 app = flask.Flask(__name__)
 
 @app.route("/upload", methods=["POST"])
 def upload():
     user_file = flask.request.files.getlist("user_file")[0]
-    new_file = handlefile(user_file)
+    new_metadata = flask.request.form
+    new_file = handlefile(user_file, flask.jsonify(new_metadata))
     return new_file
 
-def handlefile(bestandje):
-    print(bestandje)
+def handlefile(filey, tags):
+    print(tags)
+    filey.read()
+    tag = stagger.read_tag(filey)
     # doe iets met metadata
-    return bestandje
+    # {"Edit":"Submit Query","song_album":"albumpje","song_artist":"artiest",
+    # "song_number":"nummertje","song_releasedate":"","song_title":"titel"}
+
+    return flask.send_file(filey, attachment_filename = "hi")
     
-
-@app.route("/hello")
-def hello():
-    return "Hello World from Flask"
-
-
 @app.route("/")
 def main():
     index_path = os.path.join(app.static_folder, 'index.html')
